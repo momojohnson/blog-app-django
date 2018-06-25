9e60fb2e04dc11c4470e255c0448c1eb1c224eff
momojohnson <momo.johnson1987@gmail.com>
2018-06-25 16:01:18 +0000

    Made changes to topic page

--- boards/forms.py
+++ boards/forms.py
@@ -4,7 +4,7 @@ from . models import Topic, Post
 
 class NewTopicForm(forms.ModelForm):
     message = forms.CharField(
-        widget=forms.Textarea(attrs={'rows': 5, 'placeholder': 'What is on your mind?'}), 
+        widget=forms.Textarea(attrs={'rows': 10, 'placeholder': 'What is on your mind?'}), 
         max_length=4500,
         help_text='The max length of the text is 4500.')
     
